23daf8df5f6bb18cce3739e37eb0dc3ff404ef5b
Kaiyuan Deng <155346202+kaiyuan02415@users.noreply.github.com>
2024-09-01 23:24:35 -0400

    Delete LLM_Agent/EXP directory

--- LLM_Agent/EXP/README.txt
+++
@@ -1,7 +0,0 @@
-基于论文Using Large Language Models for Hyperparameter Optimization
-将基础的流程进行自动化
-
-
-
-
-
--- LLM_Agent/EXP/config.py
+++
@@ -1,3 +0,0 @@
-config = {"Max_time": 3,
-        "class_number": 10,
-        "model":"gpt-3.5-turbo"}
